806b40a79c5599238299d085c2ad93cb389a7ef1
supun-kanda <supunkandambige9@gmail.com>
2020-04-24 15:12:04 +0530

    add documentation for schemas

--- schema/schema.js
+++ schema/schema.js
@@ -20,13 +20,27 @@ const Device = require('../models/device'),
 const
     DeviceType = new GraphQLObjectType({
         name: 'Device',
+        description: "Point of sales device entity which holds its core values",
         fields: () => ({
-            id: { type: GraphQLID },
-            osVersion: { type: GraphQLString },
-            mac: { type: GraphQLString },
-            registerNo: { type: GraphQLInt },
+            id: {
+                type: GraphQLID,
+                description: "device id"
+            },
+            osVersion: {
+                type: GraphQLString,
+                description: "Operating system version of the device"
+            },
+            mac: {
+                type: GraphQLString,
+                description: "MAC address of the device"
+            },
+            registerNo: {
+                type: GraphQLInt,
+                description: "Register number given by the merchant"
+            },
             merchant: {
                 type: MerchantType,
+                description: "Merchant which owns the device",
                 resolve(parent, args) {
                     return Merchant.findById(parent.merchantId);
                 }
@@ -35,15 +49,35 @@ const
     }),
     MerchantType = new GraphQLObjectType({
         name: 'Merchant',
+        description: "Restaurant (Merchant) entity which holds restaurant properties",
         fields: () => ({
-            id: { type: GraphQLID },
-            merchantName: { type: GraphQLString },
-            location: { type: GraphQLString },
-            timeZone: { type: GraphQLString },
-            owner: { type: GraphQLString },
-            rating: { type: GraphQLFloat },
+            id: {
+                type: GraphQLID,
+                description: "Merchant Identification key"
+            },
+            merchantName: {
+                type: GraphQLString,
+                description: "Restaurant name"
+            },
+            location: {
+                type: GraphQLString,
+                description: "Restaurant location"
+            },
+            timeZone: {
+                type: GraphQLString,
+                description: "Timezone of the merchant"
+            },
+            owner: {
+                type: GraphQLString,
+                description: "The name of the owner"
+            },
+            rating: {
+                type: GraphQLFloat,
+                description: "Customer rating for the restauant"
+            },
             devices: {
                 type: new GraphQLList(DeviceType),
+                description: "Point of sales devices that merchant owns",
                 resolve(parent, args) {
                     return Device.find({ merchantId: parent.id })
                 }
@@ -54,13 +88,24 @@ const
 // queries
 const RootQuery = new GraphQLObjectType({
     name: 'Queries',
+    description: 'Queries to fetch information from the restaurants database',
     fields: {
         device: {
             type: DeviceType,
+            description: "Fetch single device information based on it's keys",
             args: {
-                deviceId: { type: GraphQLID },
-                merchantId: { type: GraphQLString },
-                registerNo: { type: GraphQLInt }
+                deviceId: {
+                    type: GraphQLID,
+                    description: "device id"
+                },
+                merchantId: {
+                    type: GraphQLString,
+                    description: "id of the merchant who owns the POS"
+                },
+                registerNo: {
+                    type: GraphQLInt,
+                    description: "Register number given by the merchant"
+                }
             },
             resolve(parent, args) {
                 const queryFeilds = ['deviceId', 'merchantId', 'registerNo'],
@@ -75,21 +120,41 @@ const RootQuery = new GraphQLObjectType({
         },
         merchant: {
             type: MerchantType,
-            args: { merchantId: { type: GraphQLID } },
+            description: "Fetch single restaurant document based on its keys",
+            args: {
+                merchantId: {
+                    type: GraphQLID,
+                    description: "id of the merchant"
+                }
+            },
             resolve(parent, args) {
                 return Merchant.findById(args.merchantId)
             }
         },
         devices: {
             type: new GraphQLList(DeviceType),
-            resolve(parent) {
-                return Device.find()
+            description: "Fetch all devices from devices collection",
+            args: {
+                limit: {
+                    type: GraphQLInt,
+                    description: "limit the responses"
+                }
+            },
+            resolve(parent, args) {
+                return Device.find().limit(args.limit)
             }
         },
         merchants: {
             type: new GraphQLList(MerchantType),
-            resolve(parent) {
-                return Merchant.find()
+            description: "Fetch all merchant documents from merchants collection",
+            args: {
+                limit: {
+                    type: GraphQLInt,
+                    description: "limit the responses"
+                }
+            },
+            resolve(parent, args) {
+                return Merchant.find().limit(args.limit)
             }
         }
     }
@@ -98,15 +163,40 @@ const RootQuery = new GraphQLObjectType({
 // mutations
 const Mutation = new GraphQLObjectType({
     name: 'Mutation',
+    description: 'CRUD operations which can be provoked on restaurant db',
     fields: {
         addMerchant: {
             type: MerchantType,
+            description: "add merchant into merchants collection restaurants db",
             args: {
-                merchantName: { type: new GraphQLNonNull(GraphQLString) },
+                merchantName: {
+                    type: new GraphQLNonNull(GraphQLString),
+                    description: "Restaurant name"
+                },
+                location: {
+                    type: GraphQLString,
+                    description: "Restaurant location"
+                },
+                timeZone: {
+                    type: GraphQLString,
+                    description: "Timezone of the merchant"
+                },
+                owner: {
+                    type: GraphQLString,
+                    description: "The name of the owner"
+                },
+                rating: {
+                    type: GraphQLFloat,
+                    description: "Customer rating for the restauant"
+                },
             },
             resolve(parent, args) {
                 const merchant = new Merchant({
-                    merchantName: args.merchantName
+                    merchantName: args.merchantName,
+                    location: args.location,
+                    timeZone: args.timeZone,
+                    owner: args.owner,
+                    rating: args.rating
                 });
                 return merchant.save()
             }
@@ -114,11 +204,30 @@ const Mutation = new GraphQLObjectType({
         addDevice: {
             type: DeviceType,
             args: {
-                deviceId: { type: new GraphQLNonNull(GraphQLID) },
-                osVersion: { type: new GraphQLNonNull(GraphQLString) },
-                registerNo: { type: new GraphQLNonNull(GraphQLInt) },
-                merchantId: { type: new GraphQLNonNull(GraphQLString) },
-                mac: { type: new GraphQLNonNull(GraphQLString) }
+                deviceId: {
+                    type: new GraphQLNonNull(GraphQLID),
+                    description: "device id"
+                },
+                osVersion: {
+                    type: new GraphQLNonNull(GraphQLString),
+                    description: "Operating system version of the device"
+
+                },
+                registerNo: {
+                    type: new GraphQLNonNull(GraphQLInt),
+                    description: "Register number given by the merchant"
+
+                },
+                merchantId: {
+                    type: new GraphQLNonNull(GraphQLString),
+                    description: "id of the merchant who owns the POS"
+
+                },
+                mac: {
+                    type: new GraphQLNonNull(GraphQLString),
+                    description: "MAC address of the device"
+
+                }
             },
             resolve(parent, args) {
                 const device = new Device({
